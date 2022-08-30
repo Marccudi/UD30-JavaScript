29a4ca7825e51f19d03aabb25d864f11c49b438d
Marccudi <45258590+Marccudi@users.noreply.github.com>
2022-08-30 13:14:58 +0200

    Add files via upload

---
+++ Task10/js/function.js
@@ -0,0 +1,12 @@
+function comprovar() {
+    let string = document.getElementById('texto').value;
+    let reves="";
+    for (let index = string.length-1; index >= 0; index--) {
+        reves+=string[index];
+    }
+    if (string == reves) {
+        alert('la cadena es palidromo');
+    } else {
+        alert('la cadena no es palidromo');
+    }
+}
